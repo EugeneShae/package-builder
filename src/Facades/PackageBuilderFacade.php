49888e8feee68e8aafa68530b111f865af80233a
Евгений Ше <evgeniy.shemena@gmail.com>
2019-10-24 23:15:24 +0300

    Added Facade class, ServiceProvider class, helpers functions

---
+++ src/Facades/PackageBuilderFacade.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Shae;
+
+class PackageBuilderFacade
+{
+
+}
---
+++ src/PackageBuilderServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Shae;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageBuilderServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //dd("PackageBuilder ready!");
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+}
