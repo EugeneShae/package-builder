78b71a4630346828d3e1e1c3a65bca5f31fda724
Евгений Ше <evgeniy.shemena@gmail.com>
2019-10-26 22:31:54 +0300

    Added some helper functions

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,15 @@
 <?php
+
+if (!function_exists('package_builder_path')){
+    function package_builder_path(string $path = ''): string
+    {
+        return __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . $path;
+    }
+}
+
+if (!function_exists('getStub')){
+    function getStub(string $name): string
+    {
+        return file_get_contents(package_builder_path('stubs' . DIRECTORY_SEPARATOR . $name . '.stub'));
+    }
+}
